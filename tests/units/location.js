e95db15270f3f93c5e3ed29d22ff0771f135d2cf
carboncollins <steven@carboncollins.uk>
2018-02-26 06:46:55 +0000

    updated tests to include fix for #2

--- tests/units/location.js
+++ tests/units/location.js
@@ -12,65 +12,67 @@ const testData = require('../templates/location.json');
 const moduleSuite = new Suite('Location module tests');
 
 moduleSuite.addTest(new Test('Exports class constructor', () => {
-  expect(UUT.name).to.be.equal('Location');
-  expect(UUT).to.be.an('function');
-  expect(new UUT()).to.be.an('object');
+  expect(UUT).to.be.an('object');
+  expect(UUT.symbols).to.be.an('object');
+  expect(UUT.class.name).to.be.equal('Location');
+  expect(UUT.class).to.be.an('function');
+  expect(new UUT.class()).to.be.an('object');
 }));
 
 moduleSuite.addTest(new Test('tiploc', () => {
-  const IUUT = new UUT(testData);
+  const IUUT = new UUT.class(testData);
   expect(IUUT.tiploc).to.not.equal(undefined);
   expect(IUUT.tiploc).to.be.an('string');
   expect(IUUT.tiploc).to.be.equal('tpl');
 }));
 
 moduleSuite.addTest(new Test('tiploc null', () => {
-  const IUUT = new UUT();
+  const IUUT = new UUT.class();
   expect(IUUT.tiploc).to.not.equal(undefined);
   expect(IUUT.tiploc).to.be.equal(null);
 }));
 
 moduleSuite.addTest(new Test('trainOperatingCompany', () => {
-  const IUUT = new UUT(testData);
+  const IUUT = new UUT.class(testData);
   expect(IUUT.trainOperatingCompany).to.not.equal(undefined);
   expect(IUUT.trainOperatingCompany).to.be.an('string');
   expect(IUUT.trainOperatingCompany).to.be.equal('toc');
 }));
 
 moduleSuite.addTest(new Test('trainOperatingCompany null', () => {
-  const IUUT = new UUT();
+  const IUUT = new UUT.class();
   expect(IUUT.trainOperatingCompany).to.not.equal(undefined);
   expect(IUUT.trainOperatingCompany).to.be.equal(null);
 }));
 
 moduleSuite.addTest(new Test('computerReservationSystem', () => {
-  const IUUT = new UUT(testData);
+  const IUUT = new UUT.class(testData);
   expect(IUUT.computerReservationSystem).to.not.equal(undefined);
   expect(IUUT.computerReservationSystem).to.be.an('string');
   expect(IUUT.computerReservationSystem).to.be.equal('crs');
 }));
 
 moduleSuite.addTest(new Test('computerReservationSystem null', () => {
-  const IUUT = new UUT();
+  const IUUT = new UUT.class();
   expect(IUUT.computerReservationSystem).to.not.equal(undefined);
   expect(IUUT.computerReservationSystem).to.be.equal(null);
 }));
 
 moduleSuite.addTest(new Test('locationName', () => {
-  const IUUT = new UUT(testData);
+  const IUUT = new UUT.class(testData);
   expect(IUUT.locationName).to.not.equal(undefined);
   expect(IUUT.locationName).to.be.an('string');
   expect(IUUT.locationName).to.be.equal('locname');
 }));
 
 moduleSuite.addTest(new Test('locationName null', () => {
-  const IUUT = new UUT();
+  const IUUT = new UUT.class();
   expect(IUUT.locationName).to.not.equal(undefined);
   expect(IUUT.locationName).to.be.equal(null);
 }));
 
 moduleSuite.addTest(new Test('updateLocation', () => {
-  const IUUT = new UUT();
+  const IUUT = new UUT.class();
   expect(IUUT.updateLocation).to.not.equal(undefined);
   expect(IUUT.updateLocation).to.be.an('function');
 
@@ -87,7 +89,7 @@ moduleSuite.addTest(new Test('updateLocation', () => {
 }));
 
 moduleSuite.addTest(new Test('updateLocation null', () => {
-  const IUUT = new UUT();
+  const IUUT = new UUT.class();
   expect(IUUT.updateLocation).to.not.equal(undefined);
   expect(IUUT.updateLocation).to.be.an('function');
 
