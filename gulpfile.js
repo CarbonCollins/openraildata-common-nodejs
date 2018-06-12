dd64ea56bdf57431c355d71d006155d7fba46442
Steven Collins <steven@carboncollins.uk>
2018-06-12 06:31:18 +0100

    re added code quality exclusions and added new preparation job

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const fs = require('fs-extra');
 const jsdoc2md = require('jsdoc-to-markdown');
 const babel = require('gulp-babel');
 const clean = require('gulp-clean');
+const rename = require('gulp-rename');
 
 gulp.task('clean-lib', () => {
   return gulp.src('lib', { read: false })
@@ -35,9 +36,20 @@ gulp.task('copy-source', ['clean-lib-es6'], () => {
     .pipe(gulp.dest('lib/es6'));
 });
 
+gulp.task('copySrcToJs', () => {
+  return gulp.src(['src/*.mjs', 'src/**/*.mjs'], { base: 'src/' })
+    .pipe(rename({
+      extname: ".js"
+    }))
+    .pipe(gulp.dest("./quality"));
+});
+
 gulp.task('compileES5', ['clean-lib-es5', 'transpile']);
 gulp.task('compileES6', ['clean-lib-es6', 'copy-source']);
 gulp.task('compile', ['compileES5', 'compileES6']);
+
+gulp.task('prepCodeQuality', ['copySrcToJs']);
+
 gulp.task('generateDocs', () => {
   return fs.ensureDir(path.join(__dirname, './docs'))
     .then(() => {
